14f9f0e2306b44556785197c177156056d20e7b3
wr112211 <wr112211.11@gmail.com>
2018-11-14 19:12:03 +0800

    ？

--- OilAnt/indent/IndentMessageViewController.swift
+++ OilAnt/indent/IndentMessageViewController.swift
@@ -8,9 +8,9 @@
 
 import UIKit
 
-class IndentMessageViewController: UIViewController, MAMapViewDelegate, AMapSearchDelegate, AMapNaviWalkManagerDelegate{
+class IndentMessageViewController: UIViewController, MAMapViewDelegate, AMapSearchDelegate, AMapNaviDriveViewDelegate,AMapNaviDriveManagerDelegate{
 
-    
+    var driveView : AMapNaviDriveView!
     var idStr = ""
     var mapView: MAMapView!
     var search: AMapSearchAPI!
@@ -55,7 +55,79 @@ class IndentMessageViewController: UIViewController, MAMapViewDelegate, AMapSear
         mapView.zoomLevel = 15
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
+        //请在 deinit 函数中执行 AMapNaviDriveManager.destroyInstance() 来销毁单例
+        
+        AMapNaviDriveManager.sharedInstance().delegate = self
+        
+        let startPoint = AMapNaviPoint.location(withLatitude: 39.993135, longitude: 116.474175)!
+        let endPoint = AMapNaviPoint.location(withLatitude: 30.908791, longitude: 116.321257)!
 
+        //设置车辆信息
+        let info = AMapNaviVehicleInfo.init()
+        info.vehicleId = "京N66Y66"; //设置车牌号
+        info.type = 1;              //设置车辆类型,0:小车; 1:货车. 默认0(小车).
+        info.size = 4;              //设置货车的类型(大小)
+        info.width = 3;             //设置货车的宽度,范围:(0,5],单位：米
+        info.height = 3.9;          //设置货车的高度,范围:(0,10],单位：米
+        info.length = 15;           //设置货车的长度,范围:(0,25],单位：米
+        info.weight = 50;           //设置货车的总重量,范围:(0,100]
+        info.load = 45;             //设置货车的核定载重,范围:(0,100],核定载重应小于总重量
+        info.axisNums = 6;          //设置货车的轴数（用来计算过路费及限重）
+        let success = AMapNaviDriveManager.sharedInstance().setVehicleInfo(info)
+        
+        AMapNaviDriveManager.sharedInstance().calculateDriveRoute(
+            withStart: [startPoint],
+            end: [endPoint],
+            wayPoints: nil,
+            drivingStrategy: AMapNaviDrivingStrategy(rawValue: 17)!)
+       
+        
+//        //起点
+//        if annotation.coordinate.latitude == self.startPointAnimation.coordinate.latitude &&  annotation.coordinate.longitude == self.startPointAnimation.coordinate.longitude {
+//            annotationView?.image = UIImage.init(named: "startPoint")
+//        }
+//        //终点
+//        if annotation.coordinate.latitude == self.endPointAnimation.coordinate.latitude && annotation.coordinate.longitude == self.endPointAnimation.coordinate.longitude{
+//            annotationView?.image = UIImage.init(named: "endPoint")
+//        }
+//  
+    }
+    
+    
+//    func driveManager(onCalculateRouteSuccess driveManager: AMapNaviDriveManager) {
+//        NSLog("CalculateRouteSuccess")
+//        //禁行信息
+//        if ((driveManager.naviRoute?.forbiddenInfo.count) != 0) {
+//            for info in (driveManager.naviRoute?.forbiddenInfo)! {
+//                NSLog("禁行信息：类型： \(info.type)，车型： \(info.vehicleType)，道路名： \(info.roadName)，禁行时间段： \(info.timeDescription)，经纬度： \(info.coordinate)")
+//            }
+//        }
+//        
+//        //限行设施
+//        if ((driveManager.naviRoute?.roadFacilityInfo.count) != 0) {
+//            for info in (driveManager.naviRoute?.roadFacilityInfo)! {
+//                NSLog("限行信息：类型： \(info.type.rawValue)，道路名： \(info.roadName)，经纬度： \(info.coordinate)")
+//            }
+//        }
+//        
+//    }
+    
+    func initDriveView() {
+        driveView = AMapNaviDriveView(frame: view.bounds)
+        driveView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        driveView.delegate = self
+        
+        view.addSubview(driveView)
+    }
+    
+    deinit {
+        AMapNaviDriveManager.sharedInstance().stopNavi()
+        AMapNaviDriveManager.sharedInstance().removeDataRepresentative(driveView)
+        AMapNaviDriveManager.sharedInstance().delegate = nil
+        
+        let success = AMapNaviDriveManager.destroyInstance()
+        NSLog("单例是否销毁成功 : \(success)")
+        
     }
     
     /*
